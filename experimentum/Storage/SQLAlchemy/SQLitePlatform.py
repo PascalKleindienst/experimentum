01456fb5a9930485ff77c16d17cc4eba1edd27ca
PascalKleindienst <mail@pascalkleindienst.de>
2018-08-17 12:13:03 +0200

    bugfixes and drop index support

--- experimentum/Storage/SQLAlchemy/SQLitePlatform.py
+++ experimentum/Storage/SQLAlchemy/SQLitePlatform.py
@@ -1,22 +1,10 @@
 from experimentum.Storage.SQLAlchemy import Platform
-from sqlalchemy.dialects.sqlite import pysqlcipher, pysqlite
 from sqlalchemy import Table, Index, ForeignKey
 
 
 class SQLitePlatform(Platform):
 
     """SQLite specific sql commands and queries."""
-
-    def is_sqlite_dialect(self):
-        """Check if dialect is a sqlite dialect.
-
-        Returns:
-            boolean
-        """
-        return (
-            isinstance(self.engine.dialect, pysqlcipher.SQLiteDialect_pysqlcipher) or
-            isinstance(self.engine.dialect, pysqlite.SQLiteDialect_pysqlite)
-        )
 
     def alter_table(self, table, cols, dropped):
         """Alter SQLite Table.
@@ -33,16 +21,17 @@ class SQLitePlatform(Platform):
         Arguments:
             table {string} -- Name of the table
             cols {list} -- List with changed columns
-            dropped {list} -- List with columns to drop
+            dropped {object} -- Object with list of columns and indexes to drop
         """
         # Prepare columns
         old_table = self.meta.tables.get(table, Table(table, self.meta, autoload=True))
-        columns = self._prepare_columns(list(old_table.columns), dropped)
+        columns = self._prepare_columns(list(old_table.columns), list(old_table.indexes), dropped)
         new_columns = columns['columns']
         new_columns.extend(cols)
 
         # * CREATE TABLE $data_table AS SELECT ($old_cols) FROM $old_table;
         data_table = self._create_table('__tmp__{}'.format(table), columns['columns'])
+        self._fill_table(data_table, columns['names'], old_table)
 
         # * DROP TABLE $old_table;
         old_table.drop(self.engine)
@@ -57,37 +46,57 @@ class SQLitePlatform(Platform):
         # * DROP TABLE $data_table;
         data_table.drop(self.engine)
 
-    def _prepare_columns(self, columns, dropped):
+    def _prepare_columns(self, columns, indexes, dropped):
         """Prepate columns so that they can be created for another table.
 
         Arguments:
             columns {list} -- Columns of a table
-            dropped {list} -- Columns to drop
+            indexes {list} -- indexes of a table
+            dropped {object} -- Columns and indexes to drop
 
         Returns:
             dict
         """
         data = {'columns': [], 'names': []}
+        dropped_fkeys = [fkey['name'] for fkey in dropped['indexes'] if fkey['type'] == 'foreign']
+        dropped_pkeys = [pkey['col'] for pkey in dropped['indexes'] if pkey['type'] == 'primary']
 
+        # Add Columns
         for col in columns:
             column = col.copy()
 
             # Add Foreign Keys
             fkeys = []
             for fkey in col.foreign_keys:
-                fkeys.append(ForeignKey(
-                    fkey.column,
-                    name=fkey.name,
-                    onupdate=fkey.onupdate,
-                    ondelete=fkey.ondelete,
-                    deferrable=fkey.deferrable
-                ))
+                if fkey.name not in dropped_fkeys:
+                    fkeys.append(ForeignKey(
+                        fkey.column,
+                        name=fkey.name,
+                        onupdate=fkey.onupdate,
+                        ondelete=fkey.ondelete,
+                        deferrable=fkey.deferrable
+                    ))
             column.foreign_keys = set(fkeys)
 
+            # Drop primary keys
+            if column.primary_key and column.name in dropped_pkeys:
+                column.primary_key = False
+
             # Add column and names
-            if column.name not in dropped:
+            if column.name not in dropped['columns']:
                 data['columns'].append(column)
                 data['names'].append(column.name)
+
+        # Add Indexes
+        for idx in indexes:
+            drop = False
+            for drop_idx in dropped['indexes']:
+                if drop_idx['name'] == idx.name:
+                    drop = True
+                    break
+
+            if drop is False:
+                data['columns'].append(idx)
 
         return data
 
@@ -117,11 +126,25 @@ class SQLitePlatform(Platform):
             Table
         """
         table = Table(tablename, self.meta, extend_existing=True)
+        used_idx = []
+
         for column in columns:
             if isinstance(column, Index):
-                table.append_constraint(column)
+                # copy columns to avoid association with new table errors
+                idx_cols = [idx_col.copy() for idx_col in column.columns]
+
+                # Add index
+                if column.name not in used_idx:
+                    table.append_constraint(Index(column.name, *idx_cols, unique=column.unique))
+                    used_idx.append(column.name)
+
+                # Drop old index if exists to avoid naming conflicts
+                try:
+                    column.drop(self.engine)
+                except Exception, e:
+                    pass
             else:
                 table.append_column(column.copy())
-        table.create(self.engine, checkfirst=True)
 
+        table.create(self.engine, checkfirst=True)
         return table
--- tests/Storage/SQLAlchemy/test_sqlite_platform.py
+++ tests/Storage/SQLAlchemy/test_sqlite_platform.py
@@ -2,18 +2,18 @@ from experimentum.Storage.SQLAlchemy import SQLitePlatform
 from sqlalchemy import Column, Text, Table, create_engine, MetaData
 from sqlalchemy import inspect, ForeignKey, Index
 
+
 class TestSQLitePlatform(object):
     @classmethod
     def setup_class(cls):
         """ init platform """
         cls.platform = SQLitePlatform()
         cls.engine = create_engine('sqlite://')
-        cls.meta = MetaData(cls.engine)
 
     def _setup_platform(self):
         self.platform.set_engine(
             self.engine,
-            self.meta
+            MetaData(self.engine)
         )
 
     def test_set_engine(self):
@@ -23,11 +23,8 @@ class TestSQLitePlatform(object):
         assert self.platform.engine is 'engine'
         assert self.platform.meta is 'meta'
 
-    def test_if_is_sqlite_dialect(self):
-        self._setup_platform()
-        assert self.platform.is_sqlite_dialect() is True
-
     def test_alter_table_add_column(self):
+        self._setup_platform()
         inspector = inspect(self.platform.engine)
         table = Table('foo', self.platform.meta, Column('bar', Text), extend_existing=True)
         table.drop(self.platform.engine, checkfirst=True)
@@ -38,10 +35,11 @@ class TestSQLitePlatform(object):
         assert len(inspector.get_columns('foo')) == 1
 
         inspector = inspect(self.platform.engine)
-        self.platform.alter_table('foo', [Column('baz', Text)], [])
+        self.platform.alter_table('foo', [Column('baz', Text)], {'columns': [], 'indexes': []})
         assert len(inspector.get_columns('foo')) == 2
 
     def test_alter_table_with_old_foreign_key(self):
+        self._setup_platform()
         inspector = inspect(self.platform.engine)
         table = Table('foobar', self.platform.meta, Column('bar', Text), extend_existing=True)
         table.drop(self.platform.engine, checkfirst=True)
@@ -57,5 +55,57 @@ class TestSQLitePlatform(object):
         assert len(inspector.get_columns('foo')) == 1
 
         inspector = inspect(self.platform.engine)
-        self.platform.alter_table('foo', [Column('baz', Text)], [])
+        self.platform.alter_table('foo', [Column('baz', Text)], {'columns': [], 'indexes': []})
         assert table.foreign_keys == set([fkey])
+
+    def test_alter_table_add_unique_index(self):
+        self._setup_platform()
+        inspector = inspect(self.platform.engine)
+        columns = [Column('bar', Text)]
+        table = Table('foo', self.platform.meta, *columns, extend_existing=True)
+        table.drop(self.platform.engine, checkfirst=True)
+        table.create(self.platform.engine)
+
+        assert 'foo' in inspector.get_table_names()
+        assert table.columns.keys() == ['bar']
+        assert len(inspector.get_columns('foo')) == 1
+        assert len(table.indexes) == 0
+
+        inspector = inspect(self.platform.engine)
+        self.platform.alter_table('foo', [Index('foo_bar_unique', *columns, unique=True)], {'columns': [], 'indexes': []})
+        assert len(table.indexes) == 1
+        assert list(table.indexes)[0].unique == 1
+
+    def test_alter_table_drop_primary(self):
+        self._setup_platform()
+        inspector = inspect(self.platform.engine)
+        table = Table('foo', self.platform.meta, Column('bar', Text, primary_key=True), extend_existing=True)
+        table.drop(self.platform.engine, checkfirst=True)
+        table.create(self.platform.engine)
+
+        assert 'foo' in inspector.get_table_names()
+        assert table.columns.keys() == ['bar']
+        assert len(inspector.get_columns('foo')) == 1
+        assert inspector.get_primary_keys('foo') == ['bar']
+
+        inspector = inspect(self.platform.engine)
+        self.platform.alter_table('foo', [], {'columns': [], 'indexes': [{'type': 'primary', 'col': 'bar', 'name': 'foo_bar_primary'}]})
+        assert inspector.get_primary_keys('foo') == []
+
+    def test_alter_table_drop_index(self):
+        self._setup_platform()
+        inspector = inspect(self.platform.engine)
+        column = Column('bar', Text)
+        table = Table('foo', self.platform.meta, column, Index('foo_bar_index', column), extend_existing=True)
+        table.drop(self.platform.engine, checkfirst=True)
+        table.create(self.platform.engine)
+
+        assert 'foo' in inspector.get_table_names()
+        assert table.columns.keys() == ['bar']
+        assert len(inspector.get_columns('foo')) == 1
+        assert inspector.get_indexes('foo')[0].get('name') == 'foo_bar_index'
+        assert inspector.get_indexes('foo')[0].get('unique') == 0
+
+        inspector = inspect(self.platform.engine)
+        self.platform.alter_table('foo', [], {'columns': [], 'indexes': [{'type': 'index', 'col': 'bar', 'name': 'foo_bar_index'}]})
+        assert inspector.get_indexes('foo') == []
