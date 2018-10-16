81caef8162e42b76a90c8e213871760014524160
Pascal Kleindienst <mail@pascalkleindienst.de>
2018-10-16 10:50:45 +0200

    added dot notation support  and methods to get and del items

--- experimentum/Experiments/DataBag.py
+++ experimentum/Experiments/DataBag.py
@@ -2,13 +2,56 @@ class DataBag(object):
     _data = {}
 
     @classmethod
-    def add(cls, key, data):
-        if key in cls._data:
-            if not isinstance(cls._data[key], list):
-                cls._data[key] = [cls._data[key]]
-            cls._data[key].append(data)
-        else:
-            cls._data[key] = data
+    def get(cls, key):
+        def get_from(items, data):
+            """Recursively get value from dictionary deep key.
+
+            Args:
+                items (list): List of dictionary keys
+                data (dict): Portion of dictionary to operate on
+
+            Returns
+                object: Value from dictionary
+
+            Raises:
+                KeyError: If key does not exist
+            """
+            it = items.pop(0)
+            data = data[it]
+            if items:
+                return get_from(items, data)
+            else:
+                return data
+
+        return get_from(key.split('.'), cls._data)
+
+    @classmethod
+    def add(cls, key, value):
+        def set_to(items, data):
+            """Recursively set value to dictionary deep key.
+
+            Args:
+                items (list): List of dictionary keys
+                data (dict): Portion of dictionary to operate on
+            """
+            item = items.pop(0)
+
+            # Traverse dictionary to last item key
+            if items:
+                if item not in data:
+                    data[item] = {}
+                set_to(items, data[item])
+            # Set Value on last key
+            else:
+                # If the key already exists and is not a list -> transfrom to list and append value
+                if item in data:
+                    if not isinstance(data[item], list):
+                        data[item] = [data[item]]
+                    data[item].append(value)
+                else:
+                    data[item] = value
+
+        set_to(key.split('.'), cls._data)
 
     @classmethod
     def merge(cls, key, data):
@@ -27,7 +70,34 @@ class DataBag(object):
         cls._data = {}
 
     @classmethod
-    def flush(cls):
+    def flush(cls, key=None):
+        if key and key in cls._data:
+            data = cls.get(key)
+            cls.delete(key)
+            return data
+
         data = cls.all()
         cls.clear()
         return data
+
+    @classmethod
+    def delete(cls, key):
+        def del_key(items, data):
+            """Recursively remove deep key from dict.
+
+            Args:
+                items (list): List of dictionary keys
+                data (dict): Portion of dictionary to operate on
+
+            Raises:
+                KeyError: If key does not exist
+            """
+            item = items.pop(0)
+            if items and item in data:
+                return del_key(items, data[item])
+            elif not items and item in data:
+                del data[item]
+            elif not items and item not in data:
+                raise KeyError(item)
+
+        del_key(key.split('.'), cls._data)
