fac4fc61948354c5fc33648909572df2b02b6cd5
Pascal Kleindienst <mail@pascalkleindienst.de>
2018-11-06 09:46:41 +0100

    update to fetch plots and display them in the plot tab

--- experimentum/WebGUI/static/event-stream.js
+++ experimentum/WebGUI/static/event-stream.js
@@ -4,16 +4,11 @@
  */
 (function($) {
     /**
-     * Close the event stream and add finish experiment run
-     * @param {EventSource} stream Event stream to close
+     * Scroll log to bottom
+     * @param {string} selector Log selector
      */
-    const finish = (stream) => {
-        stream.close()
-        $('.progress').remove();
-    }
-
-    function updateScroll(){
-        var element = $("#log .log").get(0);
+    const update_scroll = (selector) => {
+        const element = $(`#log ${selector} .log`).get(0);
         element.scrollTop = element.scrollHeight;
     }
 
@@ -21,18 +16,18 @@
      * Add an item to the log
      * @param {object} item Data item object
      */
-    const log = (item) => {
-        $('.log').append(
+    const log = (item, selector) => {
+        $(`${selector} .log`).append(
             `<pre style="margin: 0;" class="green-text"><code style="font-size: 1rem; ${item.error ? 'font-weight: bold' : ''}">${item.data}</code></pre>`
         );
 
-        updateScroll();
+        update_scroll(selector);
 
         // Error indicators
         if (item.error) {
-            $('.status i').text('error').addClass('red-text').removeClass('green-text');
-            $('.log > pre:last').removeClass('green-text').addClass('red-text');
-            $('.status-log > li.success').removeClass('success').addClass('error');
+            $(`${selector} .status i`).text('error').addClass('red-text').removeClass('green-text');
+            $(`${selector} .log > pre:last`).removeClass('green-text').addClass('red-text');
+            $(`.status-log ${selector} > li.success`).removeClass('success').addClass('error');
         }
     }
 
@@ -40,19 +35,19 @@
      * Stream the experiment log
      * @param {string} src Source url for the event stream
      */
-    const log_stream = (src) => {
+    const log_stream = (src, plot_url) => {
         const source = new EventSource(src);
 
-        source.onmessage = function(e) {
+        source.onmessage = async function(e) {
             const data = JSON.parse(e.data);
 
             // Stream started
             if (data.type == 'started') {
-                $('.loading').remove();
+                $('.experiment-log .loading').remove();
             }
             // Add data to log
             else if (data.type == 'log') {
-                log(data);
+                log(data, '.experiment-log');
             }
             // Result table
             else if (data.type == 'table') {
@@ -69,9 +64,46 @@
                 $('.config_file').html(data.data.config_file);
                 $('.config_content').html(data.data.config_content);
 
-                return finish(source);
+                await generate_plots(plot_url);
+                return source.close();
             }
         }
+    }
+
+    /**
+     * Generate plots and add them to the plot tab
+     * @param {str} url URL to endpoint for generating plots
+     */
+    const generate_plots = async (url) => {
+        $('.collapsible').collapsible('open', 1);
+
+        fetch(url, { method: 'GET' })
+            .then(response => response.json())
+            .then(messages => {
+                $('.plot-log .loading').remove();
+
+                messages.forEach(data => {
+                    // Log plot message
+                    log({
+                        data: data.message,
+                        error: !!(data.status == 'error')
+                    }, '.plot-log');
+                    
+                    // add to plot tab
+                    $('#plots .row').append(
+                        `<div class="col s12 m6">
+                            <img class="materialboxed responsive-img" src="${data.file}" />
+                        </div>`
+                    );
+                });
+
+                // Init lightbox and remove progressbar
+                $('.materialboxed').materialbox();
+                $('.progress').remove()
+            })
+            .catch(error => {
+                console.log(error)
+            });
     }
 
     // Make log stream public
