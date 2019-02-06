b5a84049c6a638cc9e2014cd810d42143a07f05b
Pascal Kleindienst <mail@pascalkleindienst.de>
2019-02-06 16:00:26 +0100

    simplified testloader and fixed path issue

--- tests/unit/Config/test_loader.py
+++ tests/unit/Config/test_loader.py
@@ -4,19 +4,16 @@ import pytest
 
 
 class TestLoader(object):
-    def _setup_loader(self, dir, fname='app.json'):
+    def _setup_loader(self, tmpdir, fname='app.json'):
         data = {'foo': 'bar'}
-        path = ''
-        with dir.join(fname).open('w+') as fh:
-            path = fh.name.replace('/' + fname, '')
+        with tmpdir.join(fname).open('w+') as fh:
             json.dump(data, fh)
 
-        self.loader = Loader(path, Config())
-        return path
+        self.loader = Loader(tmpdir.strpath, Config())
 
     def test_get_config_files(self, tmpdir):
-        path = self._setup_loader(tmpdir)
-        assert self.loader.get_config_files() == {'app': path + '/app.json'}
+        self._setup_loader(tmpdir)
+        assert self.loader.get_config_files() == {'app': tmpdir.join('app.json').strpath}
 
     def test_load_config_files_with_missing_app_conf(self, tmpdir):
         with pytest.raises(Exception):
