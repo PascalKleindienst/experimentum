0f1ea8ff05b7dfd34a29f8ebe9ce2554218be705
Pascal Kleindienst <mail@pascalkleindienst.de>
2019-02-14 15:35:36 +0100

    added cli fixture setup for functional tests

---
+++ tests/functional/conftest.py
@@ -0,0 +1,75 @@
+"""PyTest fixtures"""
+import pytest
+import tempfile
+import os
+import json
+from experimentum.Experiments import App
+from shutil import copyfile, rmtree
+from sqlalchemy.orm import clear_mappers
+
+
+def create_directories_and_files(path):
+    """Create dirs and files for the app.
+
+    Args:
+        path (str): Root Path
+    """
+    # Create Folders
+    folders = ['logs', 'experiments', 'migrations', 'repositories']
+    for folder in folders:
+        os.mkdir(os.path.join(path, folder))
+
+    # Create Init files
+    with open(os.path.join(path, 'repositories', '__init__.py'), 'w+') as outfile:
+        outfile.write('')
+
+    # Create Config Files
+    cfg = {
+        'app.json': {"prog": "main.py", "description": "Testing."},
+        'storage.json': {"datastore": {'drivername': 'sqlite', 'database': 'test.db'}, 'migrations': {'path': 'migrations'}}
+    }
+    for key, item in cfg.items():
+        with open(os.path.join(path, key), 'w') as outfile:
+            json.dump(item, outfile)
+
+    # Create Stubs
+    root = os.path.dirname(os.path.realpath(__file__))
+    stubs = [
+        {'name': '20190101000000_create_experiments', 'target': 'migrations/{name}.py'},
+        {'name': '20190101000001_create_testcase', 'target': 'migrations/{name}.py'},
+        {'name': 'ExperimentRepository', 'target': 'repositories/{name}.py'},
+        {'name': 'TestCaseRepository', 'target': 'repositories/{name}.py'},
+    ]
+    for stub in stubs:
+        copyfile(
+            os.path.join(root, '_stubs', '{}.stub'.format(stub['name'])),
+            os.path.join(path, stub['target'].replace('{name}', stub['name']))
+        )
+
+
+@pytest.fixture(scope='function')
+def cli_app():
+    """Create the cli app."""
+    # create a temporary application directory to isolate the app for each test
+    class TestContainer(App):
+        config_path = tempfile.mkdtemp()
+
+    create_directories_and_files(TestContainer.config_path)
+    container = TestContainer('testing', TestContainer.config_path + '/.')
+
+    # Init Database & clear previously mapped repos, otherwise there are some error with the mapping
+    migrator = container.make('migrator')
+    migrator.refresh()
+    clear_mappers()
+    container.bootstrap()
+
+    yield container
+
+    # Delete App and Files/Folders
+    container.store.session.close_all()
+    container.store.engine.dispose()
+    handlers = container.log.handlers[:]
+    for handler in handlers:
+        handler.close()
+        container.log.removeHandler(handler)
+    rmtree(TestContainer.config_path)
