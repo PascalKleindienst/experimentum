7676f46d4263b13e4768eda7fd50597608e44698
Pascal Kleindienst <mail@pascalkleindienst.de>
2019-04-09 16:44:43 +0200

    refactored and fixed issue doe to colorame workaround

--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -17,36 +17,29 @@ def test_print_failure_with_exit(capsys):
     assert pytest_wrapped_e.value.code == 2
 
 
-def test_get_default_input(mocker):
+@pytest.mark.parametrize('user_input,default,expected,msg', [
+    ('foo', 'default', 'foo', colored("› INPUT [default]: ", 'green')),  # optional input
+    ('', 'default', 'default', colored("› INPUT [default]: ", 'green')),  # optional input default
+    ('foo', None, 'foo', colored("› INPUT: ", 'green'))  # required input (no default)
+])
+def test_get_input(mocker, capsys, user_input, default, expected, msg):
     mock = mocker.patch('six.moves')
-    mock.input = mocker.MagicMock(return_value='foo')
-    data = get_input('INPUT', 'default')
+    mock.input = mocker.MagicMock(return_value=user_input)
+    data = get_input('INPUT', default)
 
-    mock.input.assert_called_once_with(colored("› INPUT [default]: ", 'green'))
-    assert data == 'foo'
+    mock.input.assert_called_once_with()
+    assert msg in capsys.readouterr().out
+    assert expected == data
 
-def test_get_default_input_default(mocker):
-    mock = mocker.patch('six.moves')
-    mock.input = mocker.MagicMock(return_value='')
-    data = get_input('INPUT', 'default')
-
-    mock.input.assert_called_once_with(colored("› INPUT [default]: ", 'green'))
-    assert data == 'default'
-
-def test_get_required_input(mocker):
-    mock = mocker.patch('six.moves')
-    mock.input = mocker.MagicMock(return_value='foo')
-    data = get_input('INPUT')
-
-    mock.input.assert_called_once_with(colored("› INPUT: ", 'green'))
-    assert data == 'foo'
 
 def test_get_required_input_waiting(mocker, capsys):
     mock = mocker.patch('six.moves')
     mock.input = mocker.MagicMock(side_effect=['', 'foo'])
     data = get_input('INPUT')
+    output = capsys.readouterr().out
 
-    mock.input.assert_any_call(colored("› INPUT: ", 'green'))
+    mock.input.assert_any_call()
+    assert colored("› INPUT: ", 'green') in output
     assert mock.input.call_count is 2
-    assert 'Please specify a value' in capsys.readouterr().out
+    assert 'Please specify a value' in output
     assert data == 'foo'
